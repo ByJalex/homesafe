e60e8ee5089df84e434a49a237ccb02f348e190c
ByJalex <44786998+ByJalex@users.noreply.github.com>
2020-02-27 14:51:28 -0600

    index view updated

--- app/model/productosIndexModel.php
+++ app/model/productosIndexModel.php
@@ -5,7 +5,7 @@ require_once('queryManager.php');
 
 class productos{
     public static function prPopulares(){
-        $datos = queryManager::registros('SELECT detalle_venta.id_producto AS "id_producto", SUM(cantidad) AS cantidad FROM detalle_venta GROUP BY detalle_venta.id_producto ORDER BY SUM(cantidad) DESC LIMIT 6;');
+        $datos = queryManager::registros('SELECT detalle_venta.id_producto AS "id_producto", SUM(cantidad) AS cantidad,  productos.nombre_p as "Producto", productos.precio_p as "Precio" FROM detalle_venta inner join productos on detalle_venta.id_producto = productos.id_producto GROUP BY detalle_venta.id_producto, productos.nombre_p, productos.precio_p ORDER BY SUM(cantidad) DESC LIMIT 6;');
         return $datos;
     }
 }

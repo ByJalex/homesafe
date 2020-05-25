f39719380bb0818c8eb6093e056d134d3c694834
Jalex <ayalavalencia11@gmail.com>
2020-05-25 15:41:18 -0600

    Actualización de pretty api
    - Se creo una funcion para embellecer la api.

--- app/api/product.php
+++ app/api/product.php
@@ -22,6 +22,73 @@ class product
         $sql = $con->prepare('SELECT detalle_venta.id_producto AS "id_producto", SUM(cantidad) AS cantidad,  productos.nombre_p as "Producto", productos.precio_p as "Precio", ROUND(AVG(estrellas),1) as Ranking, identificador FROM detalle_venta inner join productos on detalle_venta.id_producto = productos.id_producto inner join resenia on resenia.id_producto = productos.id_producto GROUP BY detalle_venta.id_producto, productos.nombre_p, productos.precio_p, resenia.id_producto, productos.identificador ORDER BY SUM(cantidad) DESC LIMIT 6;');
         $sql->execute();
         $getPopularProducts = $sql->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode(array('error' => false, 'popularProducts' => $getPopularProducts));
+        $jsonxD = json_encode(array('error' => false, 'popularProducts' => $getPopularProducts));
+        if($_GET['pretty']){
+            echo product::prettyPrint( $jsonxD );
+        }else{
+            echo json_encode(array('error' => false, 'popularProducts' => $getPopularProducts));
+        }
+    }
+
+    function prettyPrint($json)
+    {
+        $result = '';
+        $level = 0;
+        $in_quotes = false;
+        $in_escape = false;
+        $ends_line_level = NULL;
+        $json_length = strlen($json);
+
+        for ($i = 0; $i < $json_length; $i++) {
+            $char = $json[$i];
+            $new_line_level = NULL;
+            $post = "";
+            if ($ends_line_level !== NULL) {
+                $new_line_level = $ends_line_level;
+                $ends_line_level = NULL;
+            }
+            if ($in_escape) {
+                $in_escape = false;
+            } else if ($char === '"') {
+                $in_quotes = !$in_quotes;
+            } else if (!$in_quotes) {
+                switch ($char) {
+                    case '}':
+                    case ']':
+                        $level--;
+                        $ends_line_level = NULL;
+                        $new_line_level = $level;
+                        break;
+
+                    case '{':
+                    case '[':
+                        $level++;
+                    case ',':
+                        $ends_line_level = $level;
+                        break;
+
+                    case ':':
+                        $post = " ";
+                        break;
+
+                    case " ":
+                    case "\t":
+                    case "\n":
+                    case "\r":
+                        $char = "";
+                        $ends_line_level = $new_line_level;
+                        $new_line_level = NULL;
+                        break;
+                }
+            } else if ($char === '\\') {
+                $in_escape = true;
+            }
+            if ($new_line_level !== NULL) {
+                $result .= "\n" . str_repeat("\t", $new_line_level);
+            }
+            $result .= $char . $post;
+        }
+
+        return $result;
     }
 }
